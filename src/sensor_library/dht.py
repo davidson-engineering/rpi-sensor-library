010f28a21f9238edeeb6d4a19c2e3d2627bf619e
Matthew Davidson <matthew@davidson.engineering>
2024-03-03 23:54:45 -0800

    changed library for dht sensor

--- src/sensor_library/dht.py
+++ src/sensor_library/dht.py
@@ -1,6 +1,5 @@
 import logging
-import Adafruit_DHT
-import json
+import adafruit_dht
 
 logger = logging.getLogger(__name__)
 
@@ -8,11 +7,11 @@ logger = logging.getLogger(__name__)
 class SensorDHT11:
     def __init__(self, pin, name: str = "DHT11"):
         self.name = name
-        self.sensor = Adafruit_DHT.DHT11
+        self.sensor = adafruit_dht.DHT11
         self.pin = pin
 
     def read(self):
-        humidity, temperature = Adafruit_DHT.read(self.sensor, self.pin)
+        humidity, temperature = adafruit_dht.read(self.sensor, self.pin)
         if humidity is not None and temperature is not None:
             logger.info(
                 "Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity)
