17ca0b252ee6a76766ef3e4007214adcae331231
Matthew Davidson <matthew@davidson.engineering>
2024-03-04 20:57:43 -0800

    updated function for reading temperature

--- src/sensor_library/dht.py
+++ src/sensor_library/dht.py
@@ -7,11 +7,12 @@ logger = logging.getLogger(__name__)
 class SensorDHT11:
     def __init__(self, pin, name: str = "DHT11"):
         self.name = name
-        self.sensor = adafruit_dht.DHT11
         self.pin = pin
+        self.sensor = adafruit_dht.DHT11(pin=pin)
 
-    def read(self):
-        humidity, temperature = self.sensor.read(self.sensor, self.pin)
+    def measure(self):
+        self.sensor.measure()
+        humidity, temperature = self.sensor.humidity, self.sensor.temperature
         if humidity is not None and temperature is not None:
             logger.info(
                 "Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity)
